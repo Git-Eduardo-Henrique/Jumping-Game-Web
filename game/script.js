b6df5d72f81c647c9548f6387b568f37c55497b3
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-02 23:34:39 -0300

    game v1

---
+++ game/script.js
@@ -0,0 +1,36 @@
+const mario = document.querySelector("img.mario")
+const pipe = document.querySelector("img.pipe")
+console.log(mario, pipe)
+
+const jump = () =>{ /* muda a classe da imagem para realizar a animação */
+
+    mario.classList.add('jump')
+
+    setTimeout( /* remove a classe dps de um tempo */
+        () => {
+            mario.classList.remove('jump')
+        },
+        500
+    )
+}
+const loop = setInterval(
+    ()=>{
+        const pipeposition = pipe.offsetLeft
+        const marioposition = window.getComputedStyle(mario).bottom.replace("px", "")
+        if (pipeposition <= 120 && Number(marioposition) < 80 && pipeposition > 0){
+            pipe.style.animation = "none"
+            pipe.style.left = `${pipeposition}px`
+
+            mario.style.animation = "none"
+            mario.style.bottom = `${marioposition}px`
+
+            mario.src = "images/game-over.png"
+            mario.style.width = "75px"
+            mario.style.marginLeft = "50px"
+
+            clearInterval(loop)
+        }
+    },
+    1
+)
+document.addEventListener("keydown", jump) /* executa a função caso aperte uma tecla */
