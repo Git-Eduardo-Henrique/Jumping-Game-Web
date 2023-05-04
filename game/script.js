4ba8d8ac359d58fb9ca535761209e73c7723d0e8
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-03 22:51:45 -0300

    game death animation

--- game/script.js
+++ game/script.js
@@ -1,20 +1,20 @@
 const mario = document.querySelector("img.mario")
 const pipe = document.querySelector("img.pipe")
+const clounds = document.querySelector("img.clounds")
 
 const jump = () =>{ /* muda a classe da imagem para realizar a animação */
 
-    mario.classList.add('jump')
+    mario.classList.add('mario-jump')
 
     setTimeout( /* remove a classe dps de um tempo */
         () => {
-            mario.classList.remove('jump')
+            mario.classList.remove('mario-jump')
         },
         500
     )
 }
 const loop = setInterval( // cria um loop 
     ()=>{
-        
         const pipeposition = pipe.offsetLeft // puxa o valor da posição do cano
         const marioposition = window.getComputedStyle(mario).bottom.replace("px", "")
 
@@ -22,13 +22,18 @@ const loop = setInterval( // cria um loop
             pipe.style.animation = "none"
             pipe.style.left = `${pipeposition}px`
 
-            mario.style.animation = "none"
+            mario.style.animation = "mariodeath 1s linear"
             mario.style.bottom = `${marioposition}px`
 
             mario.src = "images/game-over.png"
-            mario.style.width = "75px"
+            mario.style.width = "5%"
             mario.style.marginLeft = "50px"
 
+            clounds.style.animation = "none"
+            setTimeout(
+                () => { mario.style.display = "none"} , 1000
+            )
+            
             clearInterval(loop)
         }
     },
