f16f5224da0e1ba548042f58045a76696f912f5d
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-11 19:20:35 -0300

    score-v1

--- game/script.js
+++ game/script.js
@@ -1,6 +1,9 @@
 const mario = document.querySelector("img.mario")
 const pipe = document.querySelector("img.pipe")
 const clounds = document.querySelector("img.clounds")
+const point = document.querySelector("span.points")
+
+var points = 0
 
 const jump = () =>{ /* muda a classe da imagem para realizar a animação */
 
@@ -13,10 +16,16 @@ const jump = () =>{ /* muda a classe da imagem para realizar a animação */
         500
     )
 }
+
 const loop = setInterval( // cria um loop 
     ()=>{
+        // almenta a pontuação
+        points += 0.1
+        point.innerHTML = `${Number.parseInt(points)} points`
+
         const pipeposition = pipe.offsetLeft // puxa o valor da posição do cano
-        const marioposition = window.getComputedStyle(mario).bottom.replace("px", "")
+        const cloundsposition = window.getComputedStyle(clounds).left.replace("px", "")
+        const marioposition = window.getComputedStyle(mario).bottom.replace("px", "") // puxa o valor do bottom
 
         if (pipeposition <= 120 && Number(marioposition) < 80 && pipeposition > 0){
             pipe.style.animation = "none"
@@ -30,6 +39,8 @@ const loop = setInterval( // cria um loop
             mario.style.marginLeft = "50px"
 
             clounds.style.animation = "none"
+            clounds.style.left = `${cloundsposition}px`
+
             setTimeout(
                 () => { mario.style.display = "none"} , 1000
             )
@@ -39,4 +50,5 @@ const loop = setInterval( // cria um loop
     },
     1
 )
+
 document.addEventListener("keydown", jump) /* executa a função caso aperte uma tecla */
