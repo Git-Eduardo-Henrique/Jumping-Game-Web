d8de1ce98101d48df8b5874e959fcead7ad4e525
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-11 20:33:05 -0300

    hi-score-v1

--- game/script.js
+++ game/script.js
@@ -2,26 +2,38 @@ const mario = document.querySelector("img.mario")
 const pipe = document.querySelector("img.pipe")
 const clounds = document.querySelector("img.clounds")
 const point = document.querySelector("span.points")
+const hipoint = document.querySelector("span.hipoints")
 
 var points = 0
+var hipoints = 0
+var int_points = 0
 
-const jump = () =>{ /* muda a classe da imagem para realizar a animação */
+const jump = (event) =>{ /* muda a classe da imagem para realizar a animação */
 
-    mario.classList.add('mario-jump')
+    if (event.keyCode === 32){
+        mario.classList.add('mario-jump')
 
-    setTimeout( /* remove a classe dps de um tempo */
-        () => {
-            mario.classList.remove('mario-jump')
-        },
-        500
-    )
+        setTimeout( /* remove a classe dps de um tempo */
+            () => {
+                mario.classList.remove('mario-jump')
+            },
+            500
+        )
+    }
 }
 
 const loop = setInterval( // cria um loop 
     ()=>{
         // almenta a pontuação
         points += 0.1
-        point.innerHTML = `${Number.parseInt(points)} points`
+        int_points = Number.parseInt(points).toString().padStart(7, "0")
+        point.innerHTML = `${int_points} points`
+
+        if (hipoints < points){
+            hipoint.innerHTML = `HI ${int_points}`
+        } 
+
+        // resto
 
         const pipeposition = pipe.offsetLeft // puxa o valor da posição do cano
         const cloundsposition = window.getComputedStyle(clounds).left.replace("px", "")
